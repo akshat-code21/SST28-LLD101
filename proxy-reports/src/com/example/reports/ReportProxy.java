30a0d0df673c8d3caa40577d4a9b2dc3bcc574af
Akshat <borncancer21@gmail.com>
2026-03-08 05:39:41 +0530

    proxy ex done

--- proxy-reports/src/com/example/reports/ReportProxy.java
+++ proxy-reports/src/com/example/reports/ReportProxy.java
@@ -1,5 +1,8 @@
 package com.example.reports;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * TODO (student):
  * Implement Proxy responsibilities here:
@@ -13,6 +16,7 @@ public class ReportProxy implements Report {
     private final String title;
     private final String classification;
     private final AccessControl accessControl = new AccessControl();
+    private RealReport realReport = null;
 
     public ReportProxy(String reportId, String title, String classification) {
         this.reportId = reportId;
@@ -20,11 +24,23 @@ public class ReportProxy implements Report {
         this.classification = classification;
     }
 
+    public RealReport getInstance(){
+        if(realReport == null){
+            realReport = new RealReport(reportId, title, classification);
+        }
+        return realReport;
+    }
+
     @Override
     public void display(User user) {
         // Starter placeholder: intentionally incorrect.
         // Students should remove direct real loading on every call.
-        RealReport report = new RealReport(reportId, title, classification);
-        report.display(user);
+        if(accessControl.canAccess(user,classification)){
+            getInstance().display(user);
+        }else {
+            System.out.println("Access denied for user: " + user.getName());
+        }
+//        RealReport report = new RealReport(reportId, title, classification);
+//        report.display(user);
     }
 }
